8d8f38efd0724b0a4911f06b2784e7b90867cad0
niolha <olhanikulina@gmail.com>
2021-11-16 12:41:23 +0100

    Add tests for function parse_parameters

--- main.py
+++ main.py
@@ -13,8 +13,19 @@ def parse_cookies(query: str) -> dict:
 if __name__ == '__main__':
     # Tests for function "parse_parameters"
     assert parse_parameters('http://example.com/?') == {}
-    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-
+    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret',
+                                                                                             'color': 'purple'}
+    assert parse_parameters('?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    try:
+        assert parse_parameters('http://example.com/path/to/page?name=ferret&color=purple:') == {'name': 'ferret',
+                                                                                                 'color': 'purple'}
+    except AssertionError:
+        print('AssertionError in the fourth test for parse_parameters!')
+    try:
+        assert parse_parameters('http://example.com/path/to/page??name=ferret&color=purple:') == {'name': 'ferret',
+                                                                                                  'color': 'purple'}
+    except AssertionError:
+        print('AssertionError in the fifth test for parse_parameters!')
     # Tests for function "parse_cookies"
     # assert parse_cookies('') == {}
     # assert parse_cookies('name=Dima;') == {'name': 'Dima'}
